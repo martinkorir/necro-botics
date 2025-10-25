7231b71fd2a5393ac43c9ce45196c479cbedd72c
Gaby Torres <torres.gabyr@gmail.com>
2025-10-25 12:46:31 -0600

    now reads ultra sonic sensor and beginning of a state machine

--- babies/simple_babies/simple_babies.ino
+++ babies/simple_babies/simple_babies.ino
@@ -11,177 +11,184 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 #include <Servo.h>
 
-// servo object representing the MG 996R servo
-
 //Baby Head Pins or wherever we hook these up to
-#define PINHEAD 0
+#define PINHEAD 9
 #define CHATTERER 2
 #define BUTTERBALL 4
 #define ANGELIQUE 6
-#define EMPTY 8
-#define EMPTY 10
 
-//total we have 12 servos
-Servo servo;
+//Sensor pins
+#define DOOR_TRIG 9
+#define DOOR_ECHO 10
+#define HALL_TRIG 11
+#define HALL_ECHO 12
+
+///-|___| hall
+///-|w
+///-|a
+///-|l
+///-|l
+///-|___/ door 
+
+//no movement, staring at wall
+#define WALL 0
+//moving towards door
+#define TO_DOOR 1
+//stopped at door
+#define DOOR 2
+//moving towards hall
+#define TO_HALL 3
+//stopped at hall
+#define HALL 4
+
+int pinheadState = WALL;
+
+//total we have 8 servos, I don't quite know how they're going to move
 Servo pinhead;
-Servo chatterer;
-Servo butterball;
-Servo angelique;
+// Servo chatterer;
+// Servo butterball;
+// Servo angelique;
 
-// State variables
-#define MOVING	LOW
-#define STOPPED HIGH
-volatile int state = STOPPED;
-volatile int edgeDetected = -1; // 0 = none, 1 = rising, -1 = falling. init'd to -1 so that it'll actually start moving
+int babies[] = {
+	PINHEAD,
+	//CHATTERER,
+	//BUTTERBALL,
+	//ANGELIQUE
+};
+Servo strollers[] = {
+	pinhead,
+	//chatterer,
+	// butterball,
+	//angelique
+};
 
-int babies[] = {PINHEAD, CHATTERER, BUTTERBALL, ANGELIQUE}
-Servo strollers[] = {pinhead, chatterer, butterball, angelique}
-
-//TODO: get the real sensor values here
-int hallSensor = 2;              // the pin that the sensor is attached to
-int doorSensor = 4;
-int val = 0;                 // variable to store the sensor status (value)
-
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
-#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
-
-// Define the servo parameters
-#define SERVO_FORWARD_PULSE_WIDTH  410
-#define SERVO_STOP_PULSE_WIDTH     0
-#define SERVO_BACKWARD_PULSE_WIDTH 205
-
-// Define servo rotation parameters
-#define STEP_DELAY    15  // Milliseconds per degree, approximate
-#define NUM_ROTATIONS 3   // How many times an animal moves back and forth
-#define STOP_DELAY    200 // How long to wait on stopped segments
-
-// our servo # counter
-uint8_t servonum = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  servo.attach(3);
-  pinhead.attach(PINHEAD);
-  chatterer.attach(CHATTERER);
-  angelique.attach(ANGELIQUE);
-  butterball.attach(BUTTERBALL);
+	// put your setup code here, to run once:
+	pinhead.attach(PINHEAD);
+	//chatterer.attach(CHATTERER);
+	//angelique.attach(ANGELIQUE);
+	//butterball.attach(BUTTERBALL);
 
-  pinMode(sensor, INPUT);    // initialize sensor as an input
-	Serial.begin(9600);        // initialize serial
+	//sensor pins
+	pinMode(DOOR_TRIG, OUTPUT);
+	pinMode(HALL_TRIG, OUTPUT);
+	pinMode(DOOR_ECHO, INPUT);
+	pinMode(HALL_ECHO, INPUT);
+
+	//pinMode(sensor, INPUT);    // initialize sensor as an input
+	Serial.begin(9600);  // initialize serial
 	Serial.println("Lets Go!");
-
-  // Register pin change interrupt for motion sensor - both edges
-	attachInterrupt (digitalPinToInterrupt (doorSensor), doorMotionISR, CHANGE);
-  attachInterrupt (digitalPinToInterrupt (hallSensor), hallMotionISR, CHANGE);
 
 	delay(10);
 }
 
 
-void doorMotionISR(){
-	if(digitalRead(doorSensor) == HIGH){
-		Serial.println("Motion detected by door");
-		edgeDetected = 1;
-		Serial.println("Rotate to this point.");
-		// stopAll(); // moved out of interrupt context
-	}
-	else{
-		Serial.println("Motion stopped.");
-		// state = MOVING; // not yet
-		edgeDetected = -1;
-		triggerTime = millis();
+// void doorMotionISR(){
+// 	if(digitalRead(doorSensor) == HIGH){
+// 		Serial.println("Motion detected by door");
+// 		edgeDetected = 1;
+// 		Serial.println("Rotate to this point.");
+// 		// stopAll(); // moved out of interrupt context
+// 	}
+// 	else{
+// 		Serial.println("Motion stopped.");
+// 		// state = MOVING; // not yet
+// 		edgeDetected = -1;
+// 		triggerTime = millis();
+// 	}
+// }
+
+// void hallMotionISR(){
+// 	if(digitalRead(hallSensor) == HIGH){
+// 		Serial.println("Motion detected by hallway");
+// 		edgeDetected = 1;
+// 		Serial.println("Rotate to this point.");
+// 		// stopAll(); // moved out of interrupt context
+// 	}
+// 	else{
+// 		Serial.println("Motion stopped.");
+// 		// state = MOVING; // not yet
+// 		edgeDetected = -1;
+// 		triggerTime = millis();
+// 	}
+// }
+
+///This is to  turn the babies head to its full range
+void wander() {
+	pinhead.write(0);    // move MG996R's shaft to angle 0°
+	delay(1000);         // wait for one second
+	pinhead.write(45);   // move MG996R's shaft to angle 45°
+	delay(1000);         // wait for one second
+	pinhead.write(90);   // move MG996R's shaft to angle 90°
+	delay(1000);         // wait for one second
+	pinhead.write(135);  // move MG996R's shaft to angle 135°
+	delay(1000);         // wait for one second
+	pinhead.write(180);  // move MG996R's shaft to angle 180°
+	delay(1000);         // wait for one second
+}
+
+long doorPreviousMillis = 0;
+long hallPreviousMillis = 0;
+long interval = 1000;
+float doorDuration, doorDistance;
+float hallDuration, hallDistance;
+
+void readDoor(unsigned long currentMillis) {
+	if (currentMillis - doorPreviousMillis > interval) {
+		// save the last time you blinked the LED
+		doorPreviousMillis = currentMillis;
+
+		digitalWrite(DOOR_TRIG, LOW);
+		delayMicroseconds(2);
+		digitalWrite(DOOR_TRIG, HIGH);
+		delayMicroseconds(10);
+		digitalWrite(DOOR_TRIG, LOW);
+
+		doorDuration = pulseIn(DOOR_ECHO, HIGH);
+		doorDistance = (doorDuration * .0343) / 2;
+		Serial.print("Door Distance: ");
+		Serial.println(doorDistance);
 	}
 }
 
-void hallMotionISR(){
-	if(digitalRead(hallSensor) == HIGH){
-		Serial.println("Motion detected by hallway");
-		edgeDetected = 1;
-		Serial.println("Rotate to this point.");
-		// stopAll(); // moved out of interrupt context
-	}
-	else{
-		Serial.println("Motion stopped.");
-		// state = MOVING; // not yet
-		edgeDetected = -1;
-		triggerTime = millis();
+void readHall(unsigned long currentMillis) {
+	if (currentMillis - hallPreviousMillis > interval) {
+		// save the last time you blinked the LED
+		hallPreviousMillis = currentMillis;
+
+		digitalWrite(HALL_TRIG, LOW);
+		delayMicroseconds(2);
+		digitalWrite(HALL_TRIG, HIGH);
+		delayMicroseconds(10);
+		digitalWrite(HALL_TRIG, LOW);
+
+		hallDuration = pulseIn(HALL_ECHO, HIGH);
+		hallDistance = (hallDuration * .0343) / 2;
+		Serial.print("Door Distance: ");
+		Serial.println(hallDistance);
 	}
 }
 
 
-#define DELAY_MS 5000
-#define MAX_DELAY_BETWEEN 7000
-int currentTime = 0;
-int triggerTime = 0;
-int timeLastMoved = 0;
-int thisDelay = MAX_DELAY_BETWEEN;
-
+//hiii ok here's what im envisioning for the states 
+// when the baby "sees" someone at the door, it should stay looking 
+//at the door until it no longer detects someone there, regardless 
+//if the hall sensor detects someone
+//when its en route to either the door or the hall, it can switch 
+//over to the door if someone is sensed, the door is the priority because 
+//thats where people enter and exit
+//the wall is its "neutral" state, we wnat itto rest there every once in a while
+//otherwise we wnat it to periodically "scan" the room, but not too often 
+//because its meant to be sneaky
+//
+//We don't want to attach any baby heads yet but essentially when we set the babies up, 
+//we'll want their 0 degree to be the center of their face since this servon is a 180 servo
+//it looks like we will have to hot glue the heads on site
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  servo.write(0);    // move MG996R's shaft to angle 0°
-  delay(1000);       // wait for one second
-  servo.write(45);   // move MG996R's shaft to angle 45°
-  delay(1000);       // wait for one second
-  servo.write(90);   // move MG996R's shaft to angle 90°
-  delay(1000);       // wait for one second
-  servo.write(135);  // move MG996R's shaft to angle 135°
-  delay(1000);       // wait for one second
-  servo.write(180);  // move MG996R's shaft to angle 180°
-  delay(1000);       // wait for one second
-
-  currentTime = millis();
-	int dTime = currentTime - triggerTime;
-  int dTimeMtn = currentTime - timeLastMoved;
-  
-	if(edgeDetected == 1){
-		stopAll();
-		state = STOPPED;
-		edgeDetected = 0;
-	}
-	if(edgeDetected == -1 && dTime > DELAY_MS){
-		state = MOVING;
-		edgeDetected = 0;
-	}
-
-	if(state == MOVING && dTimeMtn > thisDelay){
-		// Choose fast or slow ones
-		int index = random(0, 10);
-		if(index <= 2){
-      switch(index){
-        case 0:
-           moveUs(slowPins[0], slowPins[0], slowAngle);
-           break;
-        case 1:
-          moveUs(slowPins[1], slowPins[1], slowAngle);
-          break;
-        default:
-          moveUs(slowPins[0], slowPins[1], slowAngle);
-      }
-      timeLastMoved = millis();
-      thisDelay = random(0,MAX_DELAY_BETWEEN);
-		}
-		else{
-      //randomly make the animals move
-		  int A = random(0, numberFastAnimals);
-      int B = random(0, numberFastAnimals);
-      moveUs(fastPins[A], fastPins[B], fastAngle);
-      timeLastMoved = millis();
-      thisDelay = random(0,MAX_DELAY_BETWEEN);
-		}
-	}
-	else{ // ie, motion is detected
-		  // only trigger on edges - not appropriate in this scope, moved to edge detection area
-		/*
-		   Serial.println("Stopping all animals.");
-		   stopAll();
-		 */
-	}
+	unsigned long currentMillis = millis();
+	readDoor(currentMillis);
+	currentMillis = millis();
+	readHall(currentMillis);
 }
